82fa19a49b1b9281f6f91797872594b47ceba262
sireesbrl <59017772+sireesbrl@users.noreply.github.com>
2020-04-30 21:58:23 +0545

    update edi.py

--- edi.py
+++ edi.py
@@ -20,6 +20,10 @@ def open_file():
     text = f.read()      #can't insert the data from the file to the window for now
 
 
+def save():
+    pass
+
+
 def about():
     details = """
 This is a simple text editor made in python.
@@ -27,30 +31,57 @@ This is a simple text editor made in python.
     messagebox.showinfo("About", details)
 
 
-def select_fonts():
-	pass
+def select_fonts(new_font):
+    global text
+    global curr_font
+    curr_font = new_font
+    text.config(font = new_font)
 
-	
+
+def dark_mode():
+    global text
+    global curr_font
+    text.config(bg = "black", fg = "white", font = curr_font, insertbackground = "white")
+
+
+
+def light_mode():
+    global text
+    global curr_font
+    text.config(bg = "white", fg = "black", font = curr_font, insertbackground = "black")
+
+
+
 menubar = Menu(window)
 
 filemenu = Menu(menubar, tearoff = 0)
 filemenu.add_command(label = "Open", command = open_file, background = "black", foreground = "white")
-filemenu.add_command(label = "Save", command = saveas, background = "black", foreground = "white")
+filemenu.add_command(label = "Save",command = save, background = "black", foreground = "white")
+filemenu.add_command(label = "Save As", command = saveas, background = "black", foreground = "white")
 filemenu.add_command(label = "Exit", command = window.quit, background = "black", foreground = "white")
 menubar.add_cascade(label = "File", menu = filemenu, background = "black", foreground = "white")
 
 edit = Menu(menubar, tearoff = 0)
-edit.add_command(label = "Fonts", command = select_fonts, background = "black", foreground = "white")
+fonts = Menu(edit, tearoff = 0)
+interface = Menu(edit, tearoff = 0)
+fonts.add_command(label = "Arial", command = lambda:select_fonts("Arial"), background = "black", foreground = "white")
+fonts.add_command(label = "Times", command = lambda:select_fonts("Times"), background = "black", foreground = "white")
+fonts.add_command(label = "Helvetica", command = lambda:select_fonts("Helvetica"), background = "black", foreground = "white")
+interface.add_command(label = "Dark mode", command = dark_mode, background = "black", foreground = "white")
+interface.add_command(label = "Light mode", command = light_mode, background = "black", foreground = "white")
+edit.add_cascade(label = "Interface", menu = interface, background = "black", foreground = "white")
+edit.add_cascade(label = "Fonts", menu = fonts, background = "black", foreground = "white")
 menubar.add_cascade(label = "Edit", menu = edit, background = "black", foreground = "white")
 
 help_menu = Menu(menubar, tearoff = 0)
-help_menu.add_checkbutton(label = "About",command = about, background = "black", foreground = "white" )
+help_menu.add_command(label = "About",command = about, background = "black", foreground = "white" )
 menubar.add_cascade(label = "Help", menu = help_menu, background = "black", foreground = "white")
 
 window.config(menu=menubar)
 
-text= Text(window) #this line is for being able to add text in the window 
-text.config(font="Ariel")
+text= Text(window, bg = "black", fg = "white") #this line is for being able to add text in the window 
+curr_font = "Verdana"
+text.config(font = curr_font, insertbackground = "white")
 text.pack(fill = BOTH, expand = True)
 
 
